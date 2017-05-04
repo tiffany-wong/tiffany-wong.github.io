deb7ecb0814f588255acd8aefbbb8e3854c96666
Tiffany <tiff0402@hotmail.com>
2017-05-04 13:06:02 -0700

    Update myJS.js input validation

--- dist/js/myJS.js
+++ dist/js/myJS.js
@@ -1,5 +1,37 @@
 // Validating Empty Field
+// Validating Empty Field
 function check_empty() {
+if ( isEmailOk() && isNameOk() == true ) {
+alert("Form Submitted Successfully! Thank you!!");
+} else if ( isEmailOk()== false && isNameOk() == false ) {
+	document.getElementById("email").style.border = "2px solid #ff0000";
+	document.getElementById("name").style.border = "2px solid #ff0000";
+	return false;
+}
+else if (isEmailOk() == false ){
+	document.getElementById("email").style.border = "2px solid #ff0000";
+	return false; }
+
+	else if {
+	document.getElementById("name").style.border = "2px solid #ff0000";
+	return false;
+}
+}
+function isEmailOk() {
+
+        var email = document.getElementById( 'email' ).value;
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+
+}
+ function isNameOk(){
+ 	if (document.getElementById('name').value !== ""){
+ 		return true; 
+ 	} else {
+ 		return false;
+ 	}
+ }
+/*function check_empty() {
 if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
 alert("Fill All Fields!");
 } else {
@@ -7,6 +39,7 @@ document.getElementById('form').submit();
 alert("Form Submitted Successfully! Thank you!!");
 }
 }
+*/
 //Function To Display Popup
 function div_show() {
 document.getElementById('abc').style.display = "block";
