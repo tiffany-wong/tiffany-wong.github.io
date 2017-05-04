57a2120f812589e2ec38da8529530ab1b7b0ce86
Tiffany <tiff0402@hotmail.com>
2017-05-04 13:23:25 -0700

    backward changes

--- dist/js/myJS.js
+++ dist/js/myJS.js
@@ -1,37 +1,5 @@
 // Validating Empty Field
-// Validating Empty Field
 function check_empty() {
-if ( isEmailOk() && isNameOk() == true ) {
-alert("Form Submitted Successfully! Thank you!!");
-} else if ( isEmailOk()== false && isNameOk() == false ) {
-	document.getElementById("email").style.border = "2px solid #ff0000";
-	document.getElementById("name").style.border = "2px solid #ff0000";
-	return false;
-}
-else if (isEmailOk() == false ){
-	document.getElementById("email").style.border = "2px solid #ff0000";
-	return false; }
-
-	else if {
-	document.getElementById("name").style.border = "2px solid #ff0000";
-	return false;
-}
-}
-function isEmailOk() {
-
-        var email = document.getElementById( 'email' ).value;
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-
-}
- function isNameOk(){
- 	if (document.getElementById('name').value !== ""){
- 		return true; 
- 	} else {
- 		return false;
- 	}
- }
-/*function check_empty() {
 if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
 alert("Fill All Fields!");
 } else {
@@ -39,7 +7,6 @@ document.getElementById('form').submit();
 alert("Form Submitted Successfully! Thank you!!");
 }
 }
-*/
 //Function To Display Popup
 function div_show() {
 document.getElementById('abc').style.display = "block";
@@ -65,19 +32,3 @@ if (parseInt(guess) === randomNumber ) {
 
 }	
 }
-      function validateEmail(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-      }
-      function checkInputBox() {
-        var email = document.getElementById( '0xff' ).value;
-        var style = document.getElementById( '0xff' ).style; /* 'style' is an object of element with the id '0xff' */
-        var valid = validateEmail( email );
-        /* upate border of s1 */
-        if (valid == true) {
-          style.border = "2px solid #00ff00";
-        } else {
-          style.border = "2px solid #ff0000";
-        }
-      }
-
